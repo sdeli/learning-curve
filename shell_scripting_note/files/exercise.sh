d6d082e957c738f2d9e6e3f21406488628c1fee5
sdeli <bgfkszmsdeli@gmail.com>
2018-10-16 09:17:12 +0100

    committed changes before reainstall linux

---
+++ shell_scripting_note/files/exercise.sh
@@ -0,0 +1,7 @@
+#/opt/lampp/htdocs/shell_scripting_note
+
+# display the username
+USER_NAME=$(id -nu)
+
+echo "the username is $USER_NAME"
+
---
+++ shell_scripting_note/partials/sidebar-library.php
@@ -0,0 +1,9 @@
+	<aside class="col-xs-12 col-md-8">		
+							
+		<img src="files/" alt="">
+		<?php require("files/") ?>
+		<?php require("files/users/") ?>
+		<img src="img/im.png" alt="">
+		<img src="img/">
+
+	</aside>
