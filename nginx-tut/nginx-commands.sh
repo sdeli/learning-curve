94b713644a59d43bc678798596643227bf26d751
sdeli <bgfkszmsdeli@gmail.com>
2019-08-04 16:06:12 +0200

    nothing

--- nginx-tut/nginx-commands.sh
+++ nginx-tut/nginx-commands.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 sudo pkill -f nginx
 
+nginx -h
+
+sudo nginx -s stop
+sudo nginx -t -c /home/sandor/projects/learning-curve/nginx-tut/response-headers.config.sh
+sudo nginx -c /home/sandor/projects/learning-curve/nginx-tut/response-headers.config.sh
+
 # get count of cpu cores for nginx worker processes
 nproc
 lscpu
